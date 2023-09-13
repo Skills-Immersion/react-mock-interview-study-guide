dc6ba80cbbe8c702b559f9c637a0c5228e2ee622
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-09-13 11:13:08 -0700

    feature 2

--- src/ShowDetail.js
+++ src/ShowDetail.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ShowDetail({ show }) {
+  // to show and hide the type and status, we'll need
+  // - a state variable that holds whether they should be shown right now
+  // - onClick for the image that toggles that state variable
+  // - a div that conditionally shows the type & status
+  const [shouldShowExtraDetails, setShouldShowExtraDetails] = useState(false);
   return <div>
     <h3 className="title">{show.name}</h3>
-    <img src={show.image.medium} />
+    <img onClick={() => setShouldShowExtraDetails(!shouldShowExtraDetails)} src={show.image.medium} />
+    {shouldShowExtraDetails &&
+      <div>Type: {show.type} Status: {show.status}</div>}
   </div>
 }
 
