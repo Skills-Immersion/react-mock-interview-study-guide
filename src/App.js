16feabc83b93f874e9b1ebba324f0e3fb0e811dc
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-11-01 11:34:01 -0700

    day 1 of november solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import ShowDetail from './ShowDetail';
+import ShowsList from './ShowsList';
 
 function App() {
   const [shows, setShows] = useState([]);
@@ -11,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <h1>Look at these TV shows!</h1>
+      <ShowsList shows={shows} />
     </div>
   );
 }
--- src/ShowDetail.js
+++ src/ShowDetail.js
@@ -1,1 +1,18 @@
+import React, { useState } from 'react';
 
+function ShowDetail({ show }) {
+  // task 2, I need three things
+  // state variable to hold whether or not the type/status are displayed
+  // click handler on the image to toggle that state variable
+  // conditional rendering to display that data, depending on the state variable
+  const [displayStatus, setDisplayStatus] = useState(false);
+  return <div>
+    <h3 className="title">{show.name}</h3>
+    <img src={show.image.medium} onClick={() => setDisplayStatus(!displayStatus)}></img>
+    {displayStatus && <p>
+      Type: {show.type} Status: {show.status}
+    </p>}
+  </div>
+}
+
+export default ShowDetail;
